1fbeb7e9f966be7c8c95a12991e2c4255853e5de
Amitmishra010 <amit212.brh@gmail.com>
2026-07-21 00:12:04 +0530

    Build admin dashboard

---
+++ admin/vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './components/Navbar/navbar'
+import Sidebar from './components/sidebar/sidebar'
+import {Route, Routes} from 'react-router-dom'
+import Add from './pages/Add/add'
+import List from './pages/List/list'
+import Orders from './pages/Orders/orders'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+function App() {
+  const url="http://localhost:4000"
+  return (
+    <div>
+      <ToastContainer/>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+        <Route path='/add' element={<Add url={url}/>}/>
+        <Route path='/list' element={<List url={url}/>}/>
+        <Route path='/orders' element={<Orders url={url}/>}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ admin/vite-project/src/assets/assets.js
@@ -0,0 +1,14 @@
+import logo from './icon.png'
+import profile from './profile.png'
+import add_icon from './Add_icon.png'
+import order_icon from './order_icon.png'
+import upload_area from './upload_area.png'
+import parcel from './parcel.png'
+export const assets={
+    logo,
+    profile,
+    add_icon,
+    order_icon,
+    upload_area,
+    parcel
+}
